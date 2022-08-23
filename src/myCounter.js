b72821e0cbd010e5029e46b5d2687664b6364955
Daniel Oliveira <danoliveira865@gmail.com>
2022-08-22 23:46:58 -0300

    myCounter finalizado (passou no teste npm lint ok).

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,8 +19,8 @@ const myCounter = () => {
   var myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter2 = 2; counter <= 3; counter += 1) {
-      myArray.push(counter2);
+    for (let counter1 = 2; counter <= 3; counter += 1) {
+      myArray.push(counter1);
     }
   }
   return myArray;
