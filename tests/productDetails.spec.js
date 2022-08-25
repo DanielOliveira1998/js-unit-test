7c165f904036a13c10b10382d9cfbf71764aa47f
Daniel Oliveira <danoliveira865@gmail.com>
2022-08-25 00:56:56 -0300

    producDetails finalizado (passou no teste npm).

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,14 +31,19 @@ const productDetails = require('../src/productDetails');
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
+    // fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
-    expect(typeof productDetails()).toBe('function');
+    expect(typeof productDetails).toBe('function');
     // Teste se o retorno da função é um array.
+    expect(typeof productDetails ('Sabão', 'Água')).toBe('object');
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect(productDetails ('Sabão', 'Água').length).toBe(2);
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect(typeof productDetails('Sabão', 'Água') === 'object').toBeTruthy();
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect(productDetails('Sabão', 'Água')[0]).not.toEqual(productDetails('Sabão', 'Água')[1])
     // Teste se os dois productIds terminam com 123.
+    expect(productDetails('Sabão', 'Água')[0].details.productId).toContain('123');
   });
 });
